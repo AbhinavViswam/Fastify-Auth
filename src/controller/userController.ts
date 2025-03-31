9e46a077ec1d6a8498ad0a923905f5f60640fe27
Abhinav Viswam <141334489+AbhinavViswam@users.noreply.github.com>
2025-03-31 23:03:00 +0530

    prisma added

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,11 +1,25 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const Prisma = new PrismaClient();
 
 type loginBody={
     email:string,
     password:string
 }
 
-export const userLogin = (request:FastifyRequest<{Body:loginBody}>,reply:FastifyReply) =>{
+export const userLogin =async (request:FastifyRequest<{Body:loginBody}>,reply:FastifyReply) =>{
     const {email,password} = request.body
-    console.log(email,password)
+    const User = await Prisma.user.findUnique({
+        where:{email}
+    })
+    if(!User){
+        return reply.send({e:"No user found"})
+    }
+
+
+}
+
+export const userCreate = async(request:FastifyRequest,reply:FastifyReply) => {
+    
 }
